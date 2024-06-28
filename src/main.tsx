43f5af1402db0f02c10d8013fde6533efaa3f7c3
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-29 02:19:18 +0530

    setup and continue the rtk learning

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Provider } from "react-redux";
 import "./styles/app.scss"  
+import { store } from './redux/store.ts';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
---
+++ src/redux/api/UserApi.ts
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "sad" }),
+  endpoints:(builder)=>
+});
